19957d78143b6b0759572b3861a79aae94886458
br8832 <bryan2098@hotmail.com>
2020-10-27 10:01:06 -0700

    Update RegisterActivity.java

--- app/src/main/java/com/example/foodapp/RegisterActivity.java
+++ app/src/main/java/com/example/foodapp/RegisterActivity.java
@@ -77,6 +77,7 @@ public class RegisterActivity extends AppCompatActivity {
             //the next two lines add a recipe
             Recipe recipe = new  Recipe("S","I","C","K","O");
             ref.child("Recipes").child("newExample").setValue(recipe);
+
             ref.child("Users").addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot snapshot) {
