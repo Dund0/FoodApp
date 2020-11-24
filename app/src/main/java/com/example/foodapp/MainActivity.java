ab7f058ac133cb38d0374a90e447bcc27991b449
Dund0 <daniel.sanandaj@gmail.com>
2020-11-24 11:01:00 -0800

    Base layout for categories

--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -1,24 +1,18 @@
 package com.example.foodapp;
 
-import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final int RESULT_LOAD_IMAGE = 1;
-    SharedPreferences pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,19 +25,5 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
-
     }
-
-    /*
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == RESULT_LOAD_IMAGE && data !=null) {
-            pass = getApplicationContext().getSharedPreferences("currentImgView", 0);
-            Uri selectedImage = data.getData();
-            int i = pass.getInt("currentImgView", 0);
-            ImageView current = findViewById(i);
-            current.setImageURI(selectedImage);
-        }
-    }*/
 }
