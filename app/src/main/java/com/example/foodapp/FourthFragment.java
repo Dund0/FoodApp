b756e6795b2b41957e48c677a5223db771ac2e0d
Dund0 <daniel.sanandaj@gmail.com>
2021-03-09 09:51:35 -0800

    profile page now shows user posts only

--- app/src/main/java/com/example/foodapp/FourthFragment.java
+++ app/src/main/java/com/example/foodapp/FourthFragment.java
@@ -1,17 +1,33 @@
 package com.example.foodapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +35,18 @@ import com.google.firebase.auth.FirebaseUser;
  * create an instance of this fragment.
  */
 public class FourthFragment extends Fragment {
+    RecyclerView recipeRecycler;
+    ArrayList<Recipe> recipes = new ArrayList<>();
+
+    final DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+    final StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("RecipeImages");
+
+    String user;
+
+    HomePageAdapter recipeAdapter;
+
+    View rootView;
+    private boolean done = false;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -63,11 +91,82 @@ public class FourthFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        rootView = inflater.inflate(R.layout.fragment_fourth, container, false);
+        recipeRecycler = rootView.findViewById(R.id.userPosts);
+        FirebaseUser curruser = FirebaseAuth.getInstance().getCurrentUser();
+        user = curruser.getUid();
+        initList();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fourth, container, false);
+        return rootView;
+
+
     }
 
-    public void signOut(View view){
+    private void initList() {
+        StorageReference storageRef;
+
+        Query query = ref.child("Recipes");
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot rec : snapshot.getChildren()) {
+                    final Recipe rec1 = rec.getValue(Recipe.class);
+                    if (rec1.getUserId().compareTo(user) == 0) {
+                        final long ONE_MEGABYTE = 1024 * 1024;
+                        final StorageReference image = storageReference.child(rec1.title + "_image");
+                        image.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                            @Override
+                            public void onSuccess(byte[] bytes) {
+                                // Data for "---.jpg" is returns, use this as needed
+                                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                                // imagetoUpload is the imageView we want to modify
+                                rec1.setImage(bitmap);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception exception) {
+                                // Handle any errors
+                            }
+                        });
+
+                        recipes.add(rec1);
+                        assert rec1 != null;
+                    }
+                }
+
+                initRecipieRecycler(recipes);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+    private void initRecipieRecycler(final ArrayList<Recipe> recipes) {
+        recipeAdapter = new HomePageAdapter(getContext(), recipes);
+        recipeRecycler.setAdapter(recipeAdapter);
+        recipeRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        recipeAdapter.setOnItemClickListener(new HomePageAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                String s = recipes.get(position).getTitle();
+                Intent fullRec = new Intent(getContext(), HomePageItemView.class);
+                fullRec.putExtra("currentID", s);
+                startActivity(fullRec);
+            }
+
+            @Override
+            public void onDeleteClick(int position) {
+
+            }
+        });
+    }
+
+
+    public void signOut(View view) {
 
     }
 }
