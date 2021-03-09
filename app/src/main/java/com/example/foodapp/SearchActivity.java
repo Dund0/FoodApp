6b7732ef5c9cba2d91246a44f97172b069833c4a
ys514111 <youngjin.seo.963@my.csun.edu>
2021-03-08 17:45:11 -0800

    search bar implemented

--- app/src/main/java/com/example/foodapp/SearchActivity.java
+++ app/src/main/java/com/example/foodapp/SearchActivity.java
@@ -3,6 +3,13 @@ package com.example.foodapp;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
@@ -14,6 +21,8 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -32,6 +41,9 @@ public class SearchActivity extends AppCompatActivity {
 
     RecyclerView recipeRecycler;
     ArrayList<Recipe> recipes = new ArrayList<>();
+    FirebaseRecyclerOptions<Recipe> options;
+    FirebaseRecyclerAdapter<Recipe, HomePageAdapter.HomeViewHolder> adapter;
+    EditText inputSearch;
 
     final DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
     final StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("RecipeImages");
@@ -44,6 +56,46 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_activity);
+        inputSearch = (EditText) findViewById(R.id.inputSearch);
+//        inputSearch.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable editable) {
+//                if (editable.toString()!=null){
+//                    //loadData(editable.toSTring());
+//                    category = inputSearch.getText().toString();
+//                    initList();
+//                }
+//                else{
+//                    //loadData("");
+//                }
+//            }
+//        });
+        inputSearch.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+
+                if((keyEvent.getAction() == KeyEvent.ACTION_DOWN) && (i == KeyEvent.KEYCODE_ENTER)){
+                    //perform action on key press
+                    category = inputSearch.getText().toString();
+                    recipes = new ArrayList<Recipe>();
+                    initList();
+                    refreshView();
+                    return true;
+                }
+
+                return false;
+            }
+        });
 
 //        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomAppBar);
 //        NavController navController = Navigation.findNavController(this,  R.id.fragment);
@@ -99,11 +151,38 @@ public class SearchActivity extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError error) { }
         });
+
+    }
+
+   private void loadData(){
+//        final Recipe rec1;
+//        options = new FirebaseRecyclerOptions.Builder<Recipe>().setQuery(ref, Recipe.class).build();
+//        adapter = new FirebaseRecyclerAdapter<Recipe, HomePageAdapter.HomeViewHolder>(options) {
+//            @Override
+//            protected void onBindViewHolder(@NonNull HomePageAdapter.HomeViewHolder holder, int position, @NonNull Recipe model) {
+//                final long ONE_MEGABYTE = 1024 * 1024;
+//                holder.profileName.setText(model.getUserId());
+//                holder.splashDescription.setText(model.getDescription());
+//                holder.splashTags.setText((CharSequence) model.getCategories());
+//
+//            }
+//
+//            @NonNull
+//            @Override
+//            public HomePageAdapter.HomeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//                View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.home_page_item, parent, false);
+//                return new HomePageAdapter.HomeViewHolder(v);
+//            }
+//        };
     }
 
     private void initRecipieRecycler(ArrayList<Recipe> recipes) {
         recipeAdapter = new HomePageAdapter(this, recipes);
         recipeRecycler.setAdapter(recipeAdapter);
         recipeRecycler.setLayoutManager(new LinearLayoutManager(this));
+    }
+    private void refreshView(){
+        recipeRecycler.setVisibility(View.GONE);
+        recipeRecycler.setVisibility(View.VISIBLE);
     }
 }
